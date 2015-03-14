88a937f67bc9bba91948e4fc56946bd111f451ef
sunpeng196 <347744583@qq.com>
2015-03-14 16:12:15 +0800

    添加Python对象创建方式

---
+++ multiplexer.py
@@ -0,0 +1,41 @@
+﻿import collections
+import random
+
+
+class Counter(object):
+	def __init__(self,*names):
+		self.anonymous = not bool(names)
+		if self.anonymous:
+			self.count=0
+		else:
+			for name in names:
+				if not name.isidentifier():
+					raise ValueError("name must be valid identifiers")
+				setattr(self,name,0)
+	def __call__(self,event):
+		if self.anonymous:
+			self.count += event.count
+		else:
+			count=getattr(self,event.name)
+			setattr(self,event.name,count + event.count)
+			
+			
+class Event:
+	def __init__(self,name,count=1):
+		if not name.isidentifier():
+			raise ValueError("names must be valid identifiers")
+		self.name=name
+		self.count=count
+		
+class Multiplexer:
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
